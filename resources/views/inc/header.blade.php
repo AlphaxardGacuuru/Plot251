3ef1b1a188b8def13a54cc0e9377c18b23b7c4c4
Alphaxard Gacuuru <alphaxardgacuuru47@gmail.com>
2022-08-07 15:42:35 +0300

    Added icons to sliding bottom nav

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -119,36 +119,108 @@
 
         {{-- {/* Avatar Bottom */} --}}
         <div class="m-0 p-0">
-            @if(Auth::user()->name ==
-                "Admin")
-                <a href='/apartments'
-                   class="p-2">
-                    <h6><span class="ml-3 mr-4">Accounts</span></h6>
+            @guest
+            @else
+                @if(Auth::user()->name ==
+                    "Admin")
+                    <a href='/apartments'
+                       class="p-2 text-left">
+                        <h6>
+                            <span class="ml-3 mr-4">
+                                <svg xmlns="http://www.w3.org/2000/svg"
+                                     width="16"
+                                     height="16"
+                                     fill="currentColor"
+                                     class="bi bi-people"
+                                     viewBox="0 0 16 16">
+                                    <path
+                                          d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z" />
+                                </svg>
+                            </span>
+                            Accounts
+                        </h6>
+                    </a>
+                    <a href='/water-readings/create'
+                       class="p-2 text-left">
+                        <h6>
+                            <span class="ml-3 mr-4">
+                                <svg xmlns="http://www.w3.org/2000/svg"
+                                     width="16"
+                                     height="16"
+                                     fill="currentColor"
+                                     class="bi bi-files"
+                                     viewBox="0 0 16 16">
+                                    <path
+                                          d="M13 0H6a2 2 0 0 0-2 2 2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2 2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm0 13V4a2 2 0 0 0-2-2H5a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1zM3 4a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4z" />
+                                </svg>
+                            </span>
+                            Readings
+                        </h6>
+                    </a>
+                    <a href="/water-payments/create"
+                       class="p-2 text-left">
+                        <h6>
+                            <span class="ml-3 mr-4">
+                                <svg xmlns="http://www.w3.org/2000/svg"
+                                     width="16"
+                                     height="16"
+                                     fill="currentColor"
+                                     class="bi bi-wallet2"
+                                     viewBox="0 0 16 16">
+                                    <path
+                                          d="M12.136.326A1.5 1.5 0 0 1 14 1.78V3h.5A1.5 1.5 0 0 1 16 4.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 13.5v-9a1.5 1.5 0 0 1 1.432-1.499L12.136.326zM5.562 3H13V1.78a.5.5 0 0 0-.621-.484L5.562 3zM1.5 4a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-13z" />
+                                </svg>
+                            </span>
+                            Payments
+                        </h6>
+                    </a>
+                    <a href="/sms"
+                       class="p-2 text-left">
+                        <h6>
+                            <span class="ml-3 mr-4">
+                                <svg xmlns="http://www.w3.org/2000/svg"
+                                     width="16"
+                                     height="16"
+                                     fill="currentColor"
+                                     class="bi bi-chat-left-text"
+                                     viewBox="0 0 16 16">
+                                    <path
+                                          d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+                                    <path
+                                          d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z" />
+                                </svg>
+                            </span>
+                            SMS
+                        </h6>
+                    </a>
+                @endif
+                <a href="{{ route('logout') }}"
+                   class="text-left"
+                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    <h6>
+                        <span class="ml-3 mr-4">
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                 width="16"
+                                 height="16"
+                                 fill="currentColor"
+                                 class="bi bi-box-arrow-right"
+                                 viewBox="0 0 16 16">
+                                <path fill-rule="evenodd"
+                                      d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
+                                <path fill-rule="evenodd"
+                                      d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
+                            </svg>
+                        </span>
+                        {{ __('Log out') }}
+                    </h6>
                 </a>
-                <a href='/water-readings/create'
-                   class="p-2">
-                    <h6><span class="ml-3 mr-4">Readings</span></h6>
-                </a>
-                <a href="/water-payments/create"
-                   class="p-2">
-                    <h6><span class="ml-3 mr-4">Payments</span></h6>
-                </a>
-                <a href="/sms"
-                   class="p-2">
-                    <h6><span class="ml-3 mr-4">SMS</span></h6>
-                </a>
-            @endif
-            <a href="{{ route('logout') }}"
-               class="dropdown-item"
-               onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                <h6><span class="ml-3 mr-4">{{ __('Log out') }}</span></h6>
-            </a>
-            <form id="logout-form"
-                  action="{{ route('logout') }}"
-                  method="POST"
-                  style="display: none;">
-                @csrf
-            </form>
+                <form id="logout-form"
+                      action="{{ route('logout') }}"
+                      method="POST"
+                      style="display: none;">
+                    @csrf
+                </form>
+            @endguest
         </div>
         {{-- {/* Avatar Bottom End */} --}}
     </div>
